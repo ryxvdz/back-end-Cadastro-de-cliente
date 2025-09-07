68d9c7462896b9843cafe4c13bed026a407b19e1
Ryan Dias <ryanvicd2112@gmail.com>
2025-09-07 17:59:10 -0300

    update MVC

---
+++ src/test/java/com/example/crud_clientes/CrudClientesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.crud_clientes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudClientesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
